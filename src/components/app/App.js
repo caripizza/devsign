a7d5c7aac3aa40afe73b65ed9ef8408ebc426688
Cari Palazzolo <cari.pizza@gmail.com>
2019-03-13 14:51:32 -0700

    derp add Header to app

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
-// import Header from '../header/Header';
+import Header from '../header/Header';
 import { ROUTES } from '../../routes/routes';
 
 export default class App extends Component {
@@ -8,7 +8,7 @@ export default class App extends Component {
     return (
       <Router>
         < >
-          {/* <Header /> */}
+          <Header />
           <Switch>
             <Route exact component={ROUTES.HOME.Component} path={ROUTES.HOME.path} />
             <Redirect to={ROUTES.HOME.path} />

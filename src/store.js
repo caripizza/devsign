e4f1e51cad04b80e53059c9e90733ccb2f67e97d
Cari Palazzolo <cari.pizza@gmail.com>
2019-03-15 09:19:56 -0700

    tweak for netlify

--- src/store.js
+++ src/store.js
@@ -2,7 +2,11 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import reducer from './reducers';
 import { promiseMiddleware } from 'promise-middleware-redux';
 
+
+
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+
 
 export default createStore(
   reducer,
